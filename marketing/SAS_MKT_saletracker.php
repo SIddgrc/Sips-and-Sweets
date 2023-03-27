54a5386172f8f7ff01ad9ba4e87b2836dac222b1
Keithsnsn <128368594+Keithsnsn@users.noreply.github.com>
2023-03-26 19:12:27 -0700

    Update SAS_MKT_saletracker.php

--- marketing/SAS_MKT_saletracker.php
+++ marketing/SAS_MKT_saletracker.php
@@ -12,14 +12,4 @@
     echo "Date Purchased: " . $datePurchased . "<br>";
     echo "Quantity: " . $quantity . "<br>";
     echo "Payment Mode: " . $paymentMode . "<br>";
-
-<form action="edit-date.php" method="POST">
-    <input type="hidden" name="goods-type" value="<?php echo $goodsType; ?>">
-    <input type="hidden" name="item-name" value="<?php echo $itemName; ?>">
-    <input type="hidden" name="amount-cost" value="<?php echo $amountCost; ?>">
-    <input type="hidden" name="quantity" value="<?php echo $quantity; ?>">
-    <input type="hidden" name="payment-mode" value="<?php echo $paymentMode; ?>">
-    <input type="hidden" name="current-date" value="<?php echo $datePurchased; ?>">
-    <button type="submit">Edit Date Purchased</button>
-</form>
 ?>

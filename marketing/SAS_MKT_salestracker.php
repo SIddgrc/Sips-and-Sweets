d5f1b0911bf2470b9fecd592fd8ce9147aabc15c
Keithsnsn <128368594+Keithsnsn@users.noreply.github.com>
2023-03-27 22:56:25 +0800

    Update SAS_MKT_salestracker.php

--- marketing/SAS_MKT_salestracker.php
+++ marketing/SAS_MKT_salestracker.php
@@ -15,13 +15,41 @@
 
 <form action="update-date.php" method="POST">
     <label for="date-purchased">Date Purchased:</label>
-    <input type="date" id="date-purchased" name="date-purchased" value="<?php echo $_POST['current-date']; ?>">
-    <input type="hidden" name="goods-type" value="<?php echo $_POST['goods-type']; ?>">
-    <input type="hidden" name="item-name" value="<?php echo $_POST['item-name']; ?>">
-    <input type="hidden" name="amount-cost" value="<?php echo $_POST['amount-cost']; ?>">
-    <input type="hidden" name="quantity" value="<?php echo $_POST['quantity']; ?>">
-    <input type="hidden" name="payment-mode" value="<?php echo $_POST['payment-mode']; ?>">
-    <button type="submit">Update Date</button>
+    <input type="date" id="date-purchased" name="date-purchased" value="<?php echo $_POST['date-purchased']; ?>">
+    <button type="submit" name="update_date">Update Date</button>
 </form>
 
+<form action="update-goods-type.php" method="POST">
+    <label for="goods-type">Goods Type:</label>
+    <input type="text" id="goods-type" name="goods-type" value="<?php echo $_POST['goods-type']; ?>">
+    <button type="submit" name="update_goods_type">Update Goods Type</button>
+</form>
+
+<form action="update-item-name.php" method="POST">
+    <label for="item-name">Item Name:</label>
+    <input type="text" id="item-name" name="item-name" value="<?php echo $_POST['item-name']; ?>">
+    <button type="submit" name="update_item_name">Update Item Name</button>
+</form>
+
+<form action="update-amount-cost.php" method="POST">
+    <label for="amount-cost">Amount Cost:</label>
+    <input type="number" id="amount-cost" name="amount-cost" value="<?php echo $_POST['amount-cost']; ?>">
+    <button type="submit" name="update_amount_cost">Update Amount Cost</button>
+</form>
+
+<form action="update-quantity.php" method="POST">
+    <label for="quantity">Quantity:</label>
+    <input type="number" id="quantity" name="quantity" value="<?php echo $_POST['quantity']; ?>">
+    <button type="submit" name="update_quantity">Update Quantity</button>
+</form>
+
+<form action="update-payment-mode.php" method="POST">
+    <label for="payment-mode">Payment Mode:</label>
+    <select id="payment-mode" name="payment-mode">
+        <option value="cash" <?php if($_POST['payment-mode'] == 'cash') echo 'selected'; ?>>Cash</option>
+        <option value="credit" <?php if($_POST['payment-mode'] == 'credit') echo 'selected'; ?>>Credit</option>
+        <option value="debit" <?php if($_POST['payment-mode'] == 'debit') echo 'selected'; ?>>Debit</option>
+    </select>
+    <button type="submit" name="update_payment_mode">Update Payment Mode</button>
+</form>
 ?>

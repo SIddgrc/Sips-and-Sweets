58c3c0e749ce9fbe181165ea5ed7c1f8a8687c6d
Keithsnsn <128368594+Keithsnsn@users.noreply.github.com>
2023-03-26 18:50:27 -0700

    Create SAS_MKT_edit-date.php

---
+++ marketing/SAS_MKT_edit-date.php
@@ -0,0 +1,14 @@
+<?
+
+<form action="update-date.php" method="POST">
+    <label for="date-purchased">Date Purchased:</label>
+    <input type="date" id="date-purchased" name="date-purchased" value="<?php echo $_POST['current-date']; ?>">
+    <input type="hidden" name="goods-type" value="<?php echo $_POST['goods-type']; ?>">
+    <input type="hidden" name="item-name" value="<?php echo $_POST['item-name']; ?>">
+    <input type="hidden" name="amount-cost" value="<?php echo $_POST['amount-cost']; ?>">
+    <input type="hidden" name="quantity" value="<?php echo $_POST['quantity']; ?>">
+    <input type="hidden" name="payment-mode" value="<?php echo $_POST['payment-mode']; ?>">
+    <button type="submit">Update Date</button>
+</form>
+
+?>

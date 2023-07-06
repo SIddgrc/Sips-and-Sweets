703db6035e1a3ca8841f9cec0713c918cf4d83d1
SIddgrc <111411547+SIddgrc@users.noreply.github.com>
2023-07-06 15:59:37 +0800

    Delete Accounting2 directory

--- Accounting2/sas_act_new_table.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE accounting_payments (
-  id INT AUTO_INCREMENT PRIMARY KEY,
-  customer_name1 VARCHAR(255) NOT NULL,
-  transaction_date1 DATE NOT NULL,
-  amount1 DECIMAL(10, 2) NOT NULL,
-  payment_method1 VARCHAR(255) NOT NULL,
-  created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
-);
